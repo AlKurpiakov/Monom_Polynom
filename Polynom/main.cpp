e59c969cdfc7e2e8f020d8a2ec61c81d478ffc92
Aleksei Kurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2024-12-27 09:25:16 +0300

    Update main.cpp

--- Polynom/main.cpp
+++ Polynom/main.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
 #include "Polynom.h"
 
-// void foo2(){
-//     Polynom p;
-//     Monom a1 = Monom(4, 2, new int16_t[2]{3, 3});
-//     Monom a2 = Monom(1, 2, new int16_t[2]{1, 0});
-//     Monom a3 = Monom(1, 2, new int16_t[2]{0, 2});
-//     p.AppendMonom(a1);
-//     p.AppendMonom(a2);
-//     p.AppendMonom(a3);
-//     Polynom p1;
-//     Monom b1 = Monom(1, 2, new int16_t[2]{2, 0});
-//     Monom b2 = Monom(1, 2, new int16_t[2]{0, 0});
-//     p1.AppendMonom(b1);
-//     p1.AppendMonom(b2);
-//     Polynom p2 = p / p1;
-//     std::cout<<"res: ";
-//     p2.Print();
-//     std::cout << "\n\n";
-// }
-
 int main(){
     Polynom p0;
     Monom a1 = Monom(4, 2, new int16_t[2]{2,3});
